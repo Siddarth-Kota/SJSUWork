d0d9c3eba08317fa7274eaea8d850c3655dd4e11
_Kota_ <94937056+Siddarth-Kota@users.noreply.github.com>
2025-09-25 19:09:44 -0700

    fixed lab code

--- CMPE 126/Lab 3/lab3-cmpe126.cpp
+++ CMPE 126/Lab 3/lab3-cmpe126.cpp
@@ -23,27 +23,51 @@ int main() {
 
     cout << "List contents: ";
     list.print();
+    cout << endl;
 
     cout << "Is the list full? " << (list.isFull() ? "Yes" : "No") << endl;
     cout << "List size: " << list.listSize() << endl;
     cout << "Max list size: " << list.maxListSize() << endl;
 
+    cout << endl;
     ComplexNumber num6(3, 4);
     cout << "Is item at index 0 equal to (3 + 4i)? " 
          << (list.isItemAtEqual(0, num6) ? "Yes" : "No") << endl;
-
     list.removeAt(2);
     cout << "List after removing item at index 2: ";
     list.print();
+    cout << endl;
     cout << "Is the list full? " << (list.isFull() ? "Yes" : "No") << endl;
     cout << "Item at index 1: " << list.retrieveAt(1) << endl;
 
-    
+
+    ComplexNumber num7(7, 8);
+    list.replaceAt(1, num7);
+    cout << "List after replacing item at index 1 with (7 + 8i): ";
+    list.print();
+    cout << endl;
+
+    ComplexNumber num8(6, -2);
+    list.insertAt(1, num8);
+    cout << "List after inserting (6 - 2i) at index 1: ";
+    list.print();
+    cout << endl;
 
     list.clearList();
     cout << "List after clearing: ";
     list.print();
     cout << "Is the list empty? " << (list.isEmpty() ? "Yes" : "No") << endl;
+    cout << endl;
+
+    arrayList<ComplexNumber> list2(3);
+    list2.insertEnd(ComplexNumber(1, 1));
+    list2.insertEnd(ComplexNumber(2, 2));
+    list2.insertEnd(ComplexNumber(3, 3));
+
+    list = list2;
+    cout << "List after assignment from another list: ";
+    list.print();
+    cout << endl;
 
 
     return 0;

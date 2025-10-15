72be2534b23caf124ae0978a69e8d61b81a29c58
Siddarth Kota <sidkota06@gmail.com>
2025-10-15 15:52:21 -0700

    Started Lab 2

---
+++ CMPE 124/Lab 2/CMPE124-Lab2/CMPE124-Lab2.srcs/sim_1/new/tb1.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/15/2025 02:44:51 PM
+// Design Name: 
+// Module Name: tb1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb1;
+reg a,b,cin;
+wire sum,carry;
+
+fadder1bit adder(.a(a),.b(b),.cin(cin),.sum(sum),.carry(carry));
+
+initial begin
+a = 0; b = 0; cin = 0;
+#10
+a = 0; b = 0; cin = 1;
+#10
+a = 0; b = 1; cin = 0;
+#10
+a = 0; b = 1; cin = 1;
+#10
+a = 1; b = 0; cin = 0;
+#10
+a = 1; b = 0; cin = 1;
+#10
+a = 1; b = 1; cin = 0;
+#10
+a = 1; b = 1; cin = 1;
+#10
+$finish();
+end
+
+endmodule
---
+++ CMPE 124/Lab 2/CMPE124-Lab2/CMPE124-Lab2.srcs/sources_1/new/one-bit-Full-Adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/15/2025 02:34:19 PM
+// Design Name: 
+// Module Name: one-bit-Full-Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module one-bit-Full-Adder(
+
+    );
+endmodule
